597a3ae0064dc7cb0a46f71b2f333b56a8db62d1
Francisco Juan <juansoussev@gmail.com>
2026-07-17 12:08:35 -0300

    finish `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,32 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;")
-  console.log(result.rows)
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version
+
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+
+  const databaseUsedConnectionsResult = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName]
+  });
+
+  const databaseOpenedConnectionsValue = databaseUsedConnectionsResult.rows[0].count
+
   response.status(200).json({
-    chave: "valor",
-    result,
-  })
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnectionsResult.rows[0].max_connections),
+        opened_connections: databaseOpenedConnectionsValue
+      }
+    }
+  });
 }
 
 export default status
