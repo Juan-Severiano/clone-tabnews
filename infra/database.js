c4f4827a0467e57c329b853459d463f56662d048
Francisco Juan <juansoussev@gmail.com>
2026-07-23 20:51:17 -0300

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,15 +1,9 @@
 import { Client } from "pg"
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSslValues(),
-  });
+  let client;
   try {
+    client = await getNewClient()
     await client.connect();
     const result = await client.query(queryObject);
     return result;
@@ -28,9 +22,25 @@ function getSslValues() {
     }
   }
 
-  return process.env.NODE_ENV === "development" ? false : true
+  return process.env.NODE_ENV === "production" ? true : false
 }
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSslValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 
 export default {
   query,
+  getNewClient,
 }
